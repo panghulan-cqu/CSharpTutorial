94312dc407a2cf390e5be094f677463060ff770e
John mark Panghulan <johnmarkpanghulan@Johns-MacBook-Pro.local>
2023-07-12 11:33:42 +1000

    this is my first commit

---
+++ Tutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Koks!");
+            Console.WriteLine("You need to learn C#");
+            Console.WriteLine("Get ready now");
+        }
+    }
+}
