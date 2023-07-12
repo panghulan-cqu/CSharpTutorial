e8031c7bb83a2db9b6a5d3b2a8f936a1c054cfc7
John mark Panghulan <johnmarkpanghulan@Johns-MacBook-Pro.local>
2023-07-12 13:01:13 +1000

    Finish C# Strings

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -6,13 +6,7 @@
          multiple line comment*/
         static void Main(string[] args)
         {
-            // THis is a comment
-            Console.WriteLine("Hello Koks!");
-            Console.WriteLine("You need to learn C#"); //Tthis is single-line comment
-            Console.WriteLine("Get ready now");
-            Console.WriteLine(3 + 3);
-            Console.Write("Hello World! ");
-            Console.Write("I will print on the same line.");
+            Console.WriteLine(Math.Round(9.49));
         }
     }
 }
