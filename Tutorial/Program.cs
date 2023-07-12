2248626e66017368854598ab2e13fd90a3efa018
John mark Panghulan <johnmarkpanghulan@Johns-MacBook-Pro.local>
2023-07-12 11:42:33 +1000

    Finish C# output

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -2,11 +2,17 @@
 {
     class Program
     {
+        /* This is a
+         multiple line comment*/
         static void Main(string[] args)
         {
+            // THis is a comment
             Console.WriteLine("Hello Koks!");
-            Console.WriteLine("You need to learn C#");
+            Console.WriteLine("You need to learn C#"); //Tthis is single-line comment
             Console.WriteLine("Get ready now");
+            Console.WriteLine(3 + 3);
+            Console.Write("Hello World! ");
+            Console.Write("I will print on the same line.");
         }
     }
 }
